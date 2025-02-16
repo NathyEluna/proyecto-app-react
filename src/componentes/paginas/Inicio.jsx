2a4cefa87dd521e5245497b1ec435ad905f838ba
AlexaLop01 <alexandralop107@gmail.com>
2025-02-16 21:09:42 +0100

    Cambios en los estilos de cabecera , footer y el componente de inicio

--- src/componentes/paginas/Inicio.jsx
+++ src/componentes/paginas/Inicio.jsx
@@ -1,10 +1,10 @@
-
+import "../../css/Inicio.css";
 
 const Inicio = () => {
   return (
-    <div>
-      <h1>Inicio</h1>
-      <p>Bienvenido a la página de inicio</p>
+    <div className="container-inicio">
+      <h1>Bienvenidos a MindEscape</h1>
+      <p>Esta es la demo de nuestro proyecto, una plataforma web de un juego de escape room. A través de diferentes chatbots con inteligencia artificial, los jugadores podrán interactuar en distintos escenarios, resolviendo pruebas y desafíos con la ayuda de la IA para avanzar en el juego.</p>
     </div>
   );
 };
