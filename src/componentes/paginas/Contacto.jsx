db52dcb7bbc7af3b76ef338e6a5f0c058bd708a2
NathyEluna <nathcsr@gmail.com>
2025-06-04 23:27:32 +0200

    contact route changed.

--- src/componentes/paginas/Contacto.jsx
+++ src/componentes/paginas/Contacto.jsx
@@ -23,7 +23,7 @@ const Contacto = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('/api/contact', {
+      const response = await fetch('https://mindescape-seven.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
